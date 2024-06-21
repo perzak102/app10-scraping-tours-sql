0a9c52771063709191334b00efea3f392d330c2d
ubuntu <piotr.perz@wp.pl>
2024-06-21 16:50:55 +0200

    Day 43: Storing data into the file

--- main.py
+++ main.py
@@ -21,11 +21,17 @@ def store(extracted):
     with open("data.txt", "a") as file:
         file.write(extracted + "\n")
 
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
-    store(extracted)
+    content = read(extracted)
     if extracted != "No upcoming tours":
-        if extracted not in "data.txt":
-        send_email()
+        if extracted not in content:
+            send_email()
+            store(extracted)
