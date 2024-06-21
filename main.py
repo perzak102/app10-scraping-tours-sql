9f614a840f7fce020b8dcb98f8de35b0c8bf2212
ubuntu <piotr.perz@wp.pl>
2024-06-21 16:04:05 +0200

    Day 43: Extracting selector

--- main.py
+++ main.py
@@ -14,7 +14,18 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+def send_email():
+    print("Email was sent!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    store(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in "data.txt":
+        send_email()
