6fcd2bffbd4dd6c9dd417e9dbcc7febf0e245b62
ubuntu <piotr.perz@wp.pl>
2024-06-21 15:45:04 +0200

    Day 43: Extracting selector

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """Scrape the [age cource from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
