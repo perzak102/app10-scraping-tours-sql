68d6ee2655a4366b3f8aad69553b5b930b61d837
ubuntu <piotr.perz@wp.pl>
2024-06-23 22:32:27 +0200

    Day 43: Keep running program

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
+
 
 def scrape(url):
     """Scrape the [age cource from the URL"""
@@ -9,17 +13,34 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+
+    username = "mlody.inwestor.1950@gmail.com"
+    password = "fxsxzqogudhcbicn"
+
+    receiver = "perzak102@wp.pl"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
+
 
 def store(extracted):
     with open("data.txt", "a") as file:
         file.write(extracted + "\n")
+
 
 def read(extracted):
     with open("data.txt", "r") as file:
@@ -27,11 +48,14 @@ def read(extracted):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            send_email()
-            store(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message=f"New event has been found - {extracted}")
+        time.sleep(2)
+
